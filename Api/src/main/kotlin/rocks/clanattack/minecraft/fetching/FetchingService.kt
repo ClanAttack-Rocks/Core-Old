1e789d42e3fd1d253ddfa22db29b3836c92b00e7
CheeseTastisch <56408700+CheeseTastisch@users.noreply.github.com>
2023-09-26 08:38:31 +0200

    Fetching

---
+++ Api/src/main/kotlin/rocks/clanattack/minecraft/fetching/FetchingService.kt
@@ -0,0 +1,31 @@
+package rocks.clanattack.minecraft.fetching
+
+import rocks.clanattack.entry.service.Service
+import java.util.UUID
+
+/**
+ * The [FetchingService] is used to map uuids or names from mojang.
+ */
+interface FetchingService : Service {
+
+    /**
+     * Gets the uuid of a player by its [name].
+     */
+    fun getUuid(name: String): UUID?
+
+    /**
+     * Gets the name of a player by its [uuid].
+     */
+    fun getName(uuid: UUID): String?
+
+    /**
+     * Checks if a player with the given [uuid] exists.
+     */
+    fun existsUuid(uuid: UUID): Boolean
+
+    /**
+     * Checks if a player with the given [name] exists.
+     */
+    fun existsName(name: String): Boolean
+
+}
