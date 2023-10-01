a6e3d1b5944b8768a943bc219cbe40e44bbbeec6
CheeseTastisch <lian.hoerschlaeger@gmail.com>
2023-10-01 17:42:27 +0200

    Allow to print stack trace on info, warn and log without parsing a throwable

--- Impl/src/main/kotlin/rocks/clanattack/impl/util/log/Logger.kt
+++ Impl/src/main/kotlin/rocks/clanattack/impl/util/log/Logger.kt
@@ -7,19 +7,19 @@ import java.util.logging.Logger as JavaLogger
 
 class Logger(private val logger: JavaLogger) : ServiceImplementation(), Interface {
 
-    override fun info(message: String, throwable: Throwable?) {
-        if (throwable == null) logger.info(message)
-        else logger.log(Level.INFO, message, throwable)
+    override fun info(message: String, throwable: Throwable?, printStackTrace: Boolean) {
+        if (throwable != null || printStackTrace) logger.log(Level.INFO, message, throwable ?: Throwable())
+        else logger.info(message)
     }
 
-    override fun warn(message: String, throwable: Throwable?) {
-        if (throwable == null) logger.warning(message)
-        else logger.log(Level.WARNING, message, throwable)
+    override fun warn(message: String, throwable: Throwable?, printStackTrace: Boolean) {
+        if (throwable != null || printStackTrace) logger.log(Level.WARNING, message, throwable ?: Throwable())
+        else logger.warning(message)
     }
 
-    override fun error(message: String, throwable: Throwable?) {
-        if (throwable == null) logger.severe(message)
-        else logger.log(Level.SEVERE, message, throwable)
+    override fun error(message: String, throwable: Throwable?, printStackTrace: Boolean) {
+        if (throwable != null || printStackTrace) logger.log(Level.SEVERE, message, throwable ?: Throwable())
+        else logger.severe(message)
     }
 
 }
